ec9ff0309728877f7e2fea752a38e75b8fe8ece5
Mathyu Cardozo <jhon.cardozo@entel.pe>
2026-05-13 22:32:12 -0500

    Initial Commit

---
+++ Calarm/CalarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalarmApp.swift
+//  Calarm
+//
+//  Created by Mathyu Cardozo on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CalarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calarm
+//
+//  Created by Mathyu Cardozo on 13/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
